786711029d6514e605f16caa0ea507b2142af0d4
Minh Tran <mtran1712@gmail.com>
2021-04-18 14:52:32 -0700

    feat: init first component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Sample from './src/components/Sample';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Sample />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Sample/index.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Sample = () => {
+    return (
+        <View>
+            <Text>My first component init</Text>
+        </View>
+    )
+}
+
+export default Sample;
